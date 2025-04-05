28e2f57af3b22c03503c6c7dc31acaaedbe9a218
Uddeshya Singh <106103684+jettspanner123@users.noreply.github.com>
2025-04-05 19:48:04 +0530

    readme edit

---
+++ GitCommitAutomation.zsh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+
+
+#Configuration
+
+commit_interval=10
+commit_message="readme edit"
+
+
+do_commit_function() {
+  if git status --porcelain | grep .; then
+    echo "Change detected, attempthing commit...."
+    git add *
+    git commit -m "$commit_message"
+    echo "Git commit successful. Attempthing Push.."
+    git push -u origin main 
+  else 
+    echo "No Commit yet."
+  fi
+}
+
+
+while true; do 
+  do_commit_function
+  sleep "600"
+done
+
